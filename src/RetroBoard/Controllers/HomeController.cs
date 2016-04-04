5bb2bd31441e2c34139693d664c5161ff66ede62
Tojuro <thomas.j.rodgers@live.com>
2016-04-04 00:03:09 -0400

    Added code

---
+++ src/RetroBoard/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using RetroBoard.ViewModels.Home;
+
+namespace RetroBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new BoardViewModel
+            {
+                Categories = new List<CategoryModel>
+                {
+                    { new CategoryModel
+                        {
+                            Cards = new List<CardModel>(),
+                            Color = "#ffffff",
+                            Title = "Went Well"
+                        } },
+                    { new CategoryModel
+                       {
+                            Cards = new List<CardModel>(),
+                            Color = "#ffffff",
+                            Title = "Went Poorly"
+                        } },
+                    { new CategoryModel
+                     {
+                            Cards = new List<CardModel>(),
+                            Color = "#ffffff",
+                            Title = "Places to Improve"
+                        } },
+                },
+            };
+
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RetroBoard/ViewModels/Home/BoardViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetroBoard.ViewModels.Home
+{
+    public class CardModel
+    {
+        public string Text { get; set; }
+        public string Contributor { get; set; }
+    }
+
+    public class CategoryModel
+    {
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public List<CardModel> Cards { get; set; } 
+    }
+
+    public class BoardViewModel
+    {
+        public List<CategoryModel> Categories;
+    }
+}
